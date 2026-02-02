e576442b6a2b5ac3fb66456b9710b855a0a292eb
Mahmoud <ma7moudselim01@gmail.com>
2026-02-02 13:51:43 +0200

    Edit GetAll Method in Course

--- Academy.Interfaces/IServices/IAcademyServices/ICourseService.cs
+++ Academy.Interfaces/IServices/IAcademyServices/ICourseService.cs
@@ -10,9 +10,9 @@ namespace Academy.Interfaces.IServices
 	public interface ICourseService
 	{
         Task<CourseDto> AddAsync(string userId, CreateCourseDto dto);
-        Task<IEnumerable<CourseDto>> GetAllAsync();
+        Task<IEnumerable<CourseDto>> GetAllAsync(string? search = null);
         Task<CourseDto> GetByIdAsync(int id);
-        Task<IEnumerable<CourseDto>> SearchAsync(string search);
+        //Task<IEnumerable<CourseDto>> SearchAsync(string search);
         Task<CourseDto> UpdateAsync(int id, string userId, CreateCourseDto dto);
         Task DeleteAsync(int id);
         Task RestoreAsync(int id);
